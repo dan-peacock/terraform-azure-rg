e5eee3439bba042962acfd3c7e0a8f778306d1e3
danpeacock <danpeacock96@gmail.com>
2022-09-14 10:05:37 +0100

    init

---
+++ main.tf
@@ -0,0 +1,12 @@
+
+provider "azurerm" {
+  subscription_id   = var.subscription_ID
+  tenant_id         = var.tenant_ID
+  client_id         = var.SP_AppID
+  client_secret     = var.SP_Password
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.name
+  location = var.location
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "SP_Password" {
+    description = "SP Password"
+    type = string
+}
+
+variable "SP_AppID" {
+    description = "SP AppID"
+    type = string
+}
+
+variable "subscription_ID" {
+    description = "SP AppID"
+    type = string
+}
+
+variable "tenant_ID" {
+    description = "SP AppID"
+    type = string
+}
+
+variable "name" {
+  
+}
+
+variable "location" {
+  
+}
