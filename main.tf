f29be0cded72654f249760a9b9ffa90488d53b0b
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2022-09-16 16:24:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "example" {
   name     = var.name
   location = var.location
 }
+
+resource "azurerm_resource_group" "example2" {
+  name     = var.name
+  location = var.location
+}
